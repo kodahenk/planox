f69893f354637b477a51f6defb54a7af930ad25e
Orhan Osmanoğlu <orhanosmanoglu@faturamatik.com.tr>
2024-11-24 23:49:23 +0300

    task project added

---
+++ app/Models/Project.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Console\View\Components\Task;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $fillable = ['name', 'description'];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+}
